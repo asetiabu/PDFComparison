b8a0f547758caf104c0c45a986b36c88d9e6122e
asetiabu <43546314+asetiabu@users.noreply.github.com>
2019-07-12 09:33:39 -0400

    Update PDFExtractText.py

--- PDFExtractText.py
+++ PDFExtractText.py
@@ -1,12 +1,10 @@
-print ("Hello World") #test
 
 # PDF Comparison
 import PyPDF2
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-
-filename = 'C:\\users\\amadea.setiabudhi\\Desktop\\2019\\Overview of Audit US.pdf'
+filename = 'Enter File Path Here'
 
 pdfFileObj = open(filename, 'rb')
 #open allows you to read the file
@@ -19,7 +17,6 @@ count = 0
 text = ""
 #distinguishing the number of pages will allow us to parse through
 #all the pages
-
 
 pageObj = pdfReader.getPage(count)
 text = pageObj.extractText()
